7dc6e652a77e71b84bd87f040e9fa9595bdb9bc9
radonazu <rahdnus119@gmail.com>
2021-12-12 16:05:09 +0530

    Added Basic Pluggable AI
    using SOs

---
+++ My project/Assets/Scripts/StateController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour
+{
+   public State currentstate;
+   public State remaininstate;
+   public LayerMask targetlayer;
+   public Transform Target;
+   [HideInInspector]public NavMeshAgent agent;
+   public List<Transform> waypoints;
+   [HideInInspector] public int nextwaypoint=0;
+   void Awake()
+   {
+       agent=GetComponent<NavMeshAgent>();
+   }
+    void Update()
+    {
+        currentstate.UpdateState(this);
+    }
+    public void ChangetoState(State newstate)
+    {
+        if(newstate!=remaininstate)
+        {
+            currentstate=newstate;
+
+        }
+    }
+}
---
+++ My project/Assets/Scripts/Transition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Transition 
+{
+    public Decision decision;
+    public State truestate;
+    public State falsestate;
+
+    public bool TakeDecision(StateController controller)
+    {
+        return decision.decide(controller);
+    }
+}
